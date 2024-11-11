65935a380d4550f099ba963d4ef9e6d4ea43334f
admin <admin@DESKTOP-EF8RL02>
2024-11-11 12:53:57 +0200

    Добавьте файлы проекта.

---
+++ awsann.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+int main()
+{
+	int a, c, d;
+	SetConsoleOutputCP(1251);
+	cout << "Введіть c--> ";
+	cin >> c;
+	cout << "Введіть d--> ";
+	cin >> d;
+	a = c * d + 18;
+	cout << "Результат=" << a << endl;
+	return 0;
+}
